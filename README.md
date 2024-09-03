84e3ce976c0a8de5916741a271d174c680337835
Jo Murgel <dev@jomurgel.com>
2024-09-03 10:35:46 -0600

    initial app setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# t.dy
-A todo app built for the way I track tasks.
---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'jest-expo',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    globals: {
+      'ts-jest': {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    },
+  };
+  
