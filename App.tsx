12f1fb6957718ade4386db68bf3df73b0288e382
Jo Murgel <dev@jomurgel.com>
2024-09-04 18:30:22 -0600

    adds the main app view configurations

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { ReactElement } from 'react'
+import AppStack from './src/components/AppStack'
+import AuthStack from './src/components/AuthStack'
+import store from './src/redux/store'
+import { AuthProvider, useAuth } from './src/providers/AuthProvider'
+import { ThemeProvider } from './src/providers/ThemeProvider'
 
-import AppStack from './src/components/AppStack';
-import AuthStack from './src/components/AuthStack';
-import store from './src/store/store';
-import { AuthProvider, useAuth } from './src/providers/AuthProvider';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ThemeProvider } from './src/providers/ThemeProvider';
-
-export type RootStackParamList = {
-  Main: undefined;
-  Settings: undefined;
-  Splash: undefined,
-  SignUp: undefined,
-  LogIn: undefined,
-  ForgotPassword: undefined,
-  PasswordChange: undefined,
-};
-
-const AppNavigation = () => {
-  const { session } = useAuth();
-  return (session && session.user ? <AppStack /> : <AuthStack />);
+/**
+ * Determines visibility of features based on user authentication.
+ */
+function AppNavigation(): ReactElement {
+  const { session } = useAuth()
+  return ( session && session.user ? <AppStack /> : <AuthStack /> )
 }
 
-function App() {
+/**
+ * Main app entrypoint.
+ */
+function App(): ReactElement {
   return (
-    <AuthProvider>
-      <ThemeProvider>
+    <ThemeProvider>
+      <AuthProvider>
         <Provider store={store}>
           <SafeAreaProvider>
             <NavigationContainer>
@@ -34,9 +30,9 @@ function App() {
             </NavigationContainer>
           </SafeAreaProvider>
         </Provider>
-      </ThemeProvider>
-    </AuthProvider>
-  );
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
